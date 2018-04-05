66d19ac0031d2c6abe641a60fc900bc90fdbb352
Kalejo <alejo_alvarez67@hotmail.com>
2018-04-05 23:18:58 +0200

    mise à jour

---
+++ main.js
@@ -0,0 +1,9 @@
+var monsite =prompt("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+alert(monsite)
+
+
+function openModal()
+ {
+	document.getElementById
+	("modal").style.top="0px";// body...
+}
